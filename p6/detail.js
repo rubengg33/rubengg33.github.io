075f5adb8147d17e8a9e3c0aa48543b5a3716311
rubengg33 <135147356+rubengg33@users.noreply.github.com>
2024-11-27 04:46:24 +0100

    validaciones aunque no sirvieron

--- p6/detail.js
+++ p6/detail.js
@@ -22,12 +22,13 @@ const getId = () => {
 
     async render() {
       // 1. API get All Articles
-      
+
       const products = await this.loadArticles()
       console.log({ products})
       // 2. filter the 'article' by the id `this.id`
       const product = products.find(product => product.id == this.id)
       console.log({ product })
+      if (product) {
       // 3. remplace the html with the article data
       // Rellenar la plantilla con los datos del artículo
       this.querySelector('.product-title').textContent = product.title;
@@ -36,6 +37,7 @@ const getId = () => {
       this.querySelector('.product-price').textContent = product.price;
       this.querySelector('.product-rating').textContent = product.rating + '⭐';
       this.querySelector('.product-tags').textContent = product.tags;
+
       const tagsContainer = this.querySelector('.product-tags');
       tagsContainer.innerHTML = ''; // Limpia cualquier contenido previo
       product.tags.map(tag => {
@@ -52,9 +54,13 @@ const getId = () => {
         featureItem.textContent = `${feature.type.charAt(0).toUpperCase() + feature.type.slice(1)}: ${feature.value}`; // Muestra el tipo y valor
         featuresContainer.appendChild(featureItem); // Añade la característica al contenedor
       });
+      console.log('Detalles cargados correctamente');
+    } else {
+      // Si no se encuentra el producto, mostramos un mensaje
+      this.querySelector('.product-title').textContent = 'Producto no encontrado';
 
       console.log('OK')
     }
   }
-
+  }
   customElements.define('products-items', ProductItems);
